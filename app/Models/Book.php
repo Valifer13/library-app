2ee03fa8936e267c76d8f0aa023d9bafaecf7ddc
Valifer13 <vianviszello@gmail.com>
2025-03-22 20:33:06 +0800

    create book model & migration

---
+++ app/Models/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title',
+        'author',
+        'isbn',
+        'published_year',
+        'stock',
+        'category_id'
+    ];
+}
